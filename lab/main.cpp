938a417bc7ecafaceaa4ef78cadaf973ad1c992f
m3tro1d <qazwf734@gmail.com>
2022-02-14 17:51:51 +0300

    Initial commit
    Add IDE config and starting template

---
+++ lab/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using ll = long long;
+using ld = long double;
+
+void solve(std::istream& input, std::ostream& output)
+{
+}
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::ifstream input("input.txt");
+  std::ofstream output("output.txt");
+  input.tie(nullptr);
+  output.tie(nullptr);
+  
+  solve(input, output);
+
+  output.flush();
+  input.close();
+  output.close();
+}
