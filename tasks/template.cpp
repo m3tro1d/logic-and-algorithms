6dd586c66a6168f7ab481028929156a9b9a8b920
m3tro1d <qazwf734@gmail.com>
2022-03-06 09:26:50 +0300

    Add tasks template

---
+++ tasks/template.cpp
@@ -0,0 +1,31 @@
+/*
+ * Текст задания:
+ *
+ *
+ *
+ * Выполнил:
+ * Хафизов Булат, ПС-21
+ *
+ * Среда выполнения:
+ * Visual Studio 2022
+ */
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <fstream>
+#include <iostream>
+
+void Solve(std::istream& input, std::ostream& output)
+{
+}
+
+int main()
+{
+	std::ifstream input("input.txt");
+	std::ofstream output("output.txt");
+
+	input.tie(nullptr);
+	std::cin.tie(nullptr);
+	std::ios::sync_with_stdio(false);
+
+	Solve(input, output);
+}
