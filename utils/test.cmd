6e491171467aecf832f640e882d19c7980f0c30b
m3tro1d <qazwf734@gmail.com>
2022-04-08 10:29:31 +0300

    Add checking the right output in test routine

--- utils/test.cmd
+++ utils/test.cmd
@@ -7,20 +7,23 @@ if %subject% == "" (
 )
 
 FOR %%f in (tests\*.in) DO (
-  COPY %%f input.txt > nul
+  copy %%f input.txt > nul
+  copy "tests\%%~nf.out" output_correct.txt > nul
+
   %subject% > nul || goto failed
   fc output.txt "tests\%%~nf.out" > nul || goto failed
+
   echo Test %%f passed
 )
 
 echo.
 echo All tests passed
-del input.txt output.txt
+del input.txt output.txt output_correct.txt
 exit /B 0
 
 :failed
 echo.
-echo Test failed. Check input.txt and output.txt
+echo Test failed. Check input.txt, output.txt and output_correct.txt
 exit /B 1
 
 :error
